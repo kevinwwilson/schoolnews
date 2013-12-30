2d0146de792037b01fe4588d10ab3be6068123a9
kevinwwilson <kwilson@foxbright.com>
2013-12-30 16:16:03 -0500

    Added custom theme paths and formatted search results

--- config/site_theme_paths.php
+++ config/site_theme_paths.php
@@ -12,11 +12,17 @@ defined('C5_EXECUTE') or die(_("Access Denied."));
 
 */
 
-/*
 $v = View::getInstance();
 
-$v->setThemeByPath('/login', "yourtheme");
-$v->setThemeByPath('/page_forbidden', "yourtheme");
-$v->setThemeByPath('/register', "yourtheme");
-
-*/
+$v->setThemeByPath('/login', "kent-news"); 
+$v->setThemeByPath('/403', "kent-news");
+$v->setThemeByPath('/register', "kent-news");
+$v->setThemeByPath('/download_file', "kent-news");
+$v->setThemeByPath('/install', "kent-news");
+$v->setThemeByPath('/maintenance_mode', "kent-news");
+$v->setThemeByPath('/members', "kent-news");
+$v->setThemeByPath('/page_forbidden', "kent-news");
+$v->setThemeByPath('/upgrade', "kent-news");
+$v->setThemeByPath('/maintenance_mode', "kent-news");
+$v->setThemeByPath('/page_not_found', "kent-news");
+$v->setThemeByPath('/user_error', "kent-news");
