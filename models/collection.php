ec5034707706443c8a728393160ea303dd38fbd0
kevinwwilson <kwilson@foxbright.com>
2014-01-16 15:57:45 -0500

    Added regional feature to the things manually forced into the attribute search index.

--- models/collection.php
+++ models/collection.php
@@ -29,15 +29,22 @@ class Collection extends Concrete5_Model_Collection {
                                 // added this as a hack - concrete 5 was not cooperating with setting this in the search index
                                 $page = Page::getByID($this->getCollectionID());
                                 $ak_g = CollectionAttributeKey::getByHandle('group_status');
+                                $ak_r = CollectionAttributeKey::getByHandle('regional_feature');
+                                          
                                 $status = $page->getAttribute($ak_g);
+                                $regional_feature = $page->getAttribute($ak_r);
                                 
-                                //This is guaranteed to be an array of 1 by the select type
+                                //These are both guaranteed to be an array of 1 by the select type
                                 foreach ($status as $v) {
                                     $stat = $v->value;
                                 }
                                 
+                                foreach ($regional_feature as $r) {
+                                    $regional = $r->value;
+                                }
+                                
                                 //force the value into the search index
-                                $db->Replace('collectionsearchindexattributes', array('cID'=>$this->getCollectionID(), 'ak_group_status'=>$stat), array("cID"), true);
+                                $db->Replace('collectionsearchindexattributes', array('cID'=>$this->getCollectionID(), 'ak_group_status'=>$stat, 'ak_regional_feature' => $regional), array("cID"), true);
                                 //  End hack of core code
                                 
 				$cache = PageCache::getLibrary();
