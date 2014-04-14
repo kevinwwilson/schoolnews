a2462471171d7dcce910a9bae824f52189c82dbc
Kevin Wilson <kwilson@fh.org>
2014-04-14 07:32:12 -0400

    Added news list template that hides top article

--- blocks/pronews_list/controller.php
+++ blocks/pronews_list/controller.php
@@ -1,4 +1,4 @@
-<?php  
+<?php
 
 	defined('C5_EXECUTE') or die(_("Access Denied."));
 	class PronewsListBlockController extends BlockController {
@@ -6,24 +6,24 @@
 		protected $btTable = 'btProNewsList';
 		protected $btInterfaceWidth = "500";
 		protected $btInterfaceHeight = "350";
-		
-		/** 
+
+		/**
 		 * Used for localization. If we want to localize the name/description we have to include this
 		 */
 		public function getBlockTypeDescription() {
 			return t("List News Items based on type, area, or category.");
 		}
-		
+
 		public function getBlockTypeName() {
 			return t("ProNews List");
 		}
-		
+
 		public function getJavaScriptStrings() {
 			return array(
 				'feed-name' => t('Please give your RSS Feed a name.')
 			);
 		}
-		
+
 		function getPages($query = null) {
 			Loader::model('page_list');
 			$db = Loader::db();
@@ -45,11 +45,11 @@
 				$row['displayAliases'] = $this->displayAliases;
 				$row['title'] = $this->title;
 			}
-			
+
 
 			$pl = new PageList();
 			$pl->setNameSpace('b' . $this->bID);
-			
+
 			$cArray = array();
 
 			switch($row['orderBy']) {
@@ -74,7 +74,7 @@
 			}
 
 			$num = (int) $row['num'];
-			
+
 			if ($num > 0) {
 				$pl->setItemsPerPage($num);
 			}
@@ -84,14 +84,14 @@
 				$this->cID = $c->getCollectionID();
 			}
 			$cParentID = ($row['cThis']) ? $this->cID : $row['cParentID'];
-			
+
 			if($this->displayArchive == 1){
 				$date = date('Y-m-d',strtotime('-1 days',strtotime(date('Y-m-d'))));
 				$pl->filterByPublicDate($date,'<=');
 			}else{
-				$pl->filterByPublicDate(date('Y-m-d H:i:s'),'<=');	
+				$pl->filterByPublicDate(date('Y-m-d H:i:s'),'<=');
 			}
-			
+
 			Loader::model('attribute/categories/collection');
 			if ($this->displayFeaturedOnly == 1) {
 				$cak = CollectionAttributeKey::getByHandle('is_featured');
@@ -99,12 +99,12 @@
 					$pl->filterByIsFeatured(1);
 				}
 			}
-			
+
 			if (!$row['displayAliases']) {
 				$pl->filterByIsAlias(0);
 			}
-			$pl->filter('cvName', '', '!=');			
-		
+			$pl->filter('cvName', '', '!=');
+
 			if ($row['ctID']) {
 				$pl->filterByCollectionTypeID($row['ctID']);
 			}
@@ -113,155 +113,149 @@
 			if (isset($columns['AK_EXCLUDE_PAGE_LIST'])) {
 				$pl->filter(false, '(ak_exclude_page_list = 0 or ak_exclude_page_list is null)');
 			}
-			
+
 			if ( intval($row['cParentID']) != 0) {
 				$pl->filterByParentID($cParentID);
 			}
-			
-			if ($this->category != 'All Categories') {				
+
+			if ($this->category != 'All Categories') {
 				$category = "\n$this->category\n";
-				//echo $this->category;
-				//print_r($pl->getAttributeValue('news_category'));
-				//print_r($category);
 				$pl->filterByAttribute('news_category', "%$category%", 'LIKE');
-				//$pl->filterByAttribute('news_category',"%\n$category\n%",'like');
-				//$pl->filterByNewsCategory($category,'LIKE');
 			}
-			
-			
-			
-			if ($this->tagss != 'All Tags') {				
-				$tagss = "\n$this->tagss\n";				
+
+
+			if ($this->tagss != 'All Tags') {
+				$tagss = "\n$this->tagss\n";
 				//$pl->filterByAttribute('news_category', "%\n$category\n%", 'like');
 				$pl->filterByAttribute('news_tag',"%$tagss%",'like');
 				//$pl->filterByNewsCategory($category,'LIKE');
 			}
-							
-			
-			if ($this->distss != 'All District') {				
-				$distss = "\n$this->distss\n";				
+
+			if ($this->distss != 'All District') {
+				$distss = "\n$this->distss\n";
 				$pl->filterByAttribute('district',"%$distss%",'like');
-				
+
 			}
-			
-			
+
+
 			$b = Block::getByID($this->bID);
-            $template = strtolower($b->getBlockFilename());
-			
-			if($template=='home_images'){								
-			$pl->filterByAttribute('regional_feature',"%$this->category%",'like');			
+
+                        $template = strtolower($b->getBlockFilename());
+
+			if($template=='home_images'){
+			$pl->filterByAttribute('regional_feature',"%$this->category%",'like');
 			}
-			
-			
-			if($template=='left_side'){	
+
+			if($template=='left_side'){
 			global $u;
-			$pl->filterByAttribute('regional_feature',"%$this->category%",'like');			
+			$pl->filterByAttribute('regional_feature',"%$this->category%",'like');
 			}
-			
-                        if($template=='district_left_side'){	
-                        $pl->filter(false,"ak_group_status like '%Published%'"); 
+
+                        if($template=='district_left_side'){
+                        $pl->filter(false,"ak_group_status like '%Published%'");
 			}
-			
-			
-			if($template=='full_list'){	
+
+			if($template=='full_list'){
+                                        global $u;
+                            if (!$u -> isLoggedIn ()) {
+                            $pl->filter(false,"ak_group_status like '%Published%'");
+
+                            }
+                            else{
+                                    $pl->filter(false,"(ak_group_status like '%Published%' or ak_group_status like '%Ready%')");
+                                }
+			}
+
+
+			if($template=='home_main'){
 			global $u;
-            if (!$u -> isLoggedIn ()) {
-            $pl->filter(false,"ak_group_status like '%Published%'");            
-                                    
-            }
-            else{
-		    $pl->filter(false,"(ak_group_status like '%Published%' or ak_group_status like '%Ready%')");            
-	        }		
+                            if (!$u -> isLoggedIn ()) {
+                            $pl->filter(false,"ak_group_status like '%Published%'");
+
+                            }
+                            else{
+                                    $pl->filter(false,"ak_group_status like '%Published%' or ak_group_status like '%Ready%'");
+                                }
 			}
-			
-			
-			if($template=='home_main'){	
+
+			if($template=='home_title'){
 			global $u;
-            if (!$u -> isLoggedIn ()) {
-            $pl->filter(false,"ak_group_status like '%Published%'");            
-                                    
-            }
-            else{
-		    $pl->filter(false,"ak_group_status like '%Published%' or ak_group_status like '%Ready%'");	            
-	        }					
+                            if (!$u -> isLoggedIn ()) {
+                            $pl->filter(false,"ak_group_status like '%Published%'");
+
+                            }
+                            else{
+                                    $pl->filter(false,"ak_group_status like '%Published%' or ak_group_status like '%Ready%'");
+                                }
 			}
-			
-			if($template=='home_title'){	
-			global $u;
-            if (!$u -> isLoggedIn ()) {
-            $pl->filter(false,"ak_group_status like '%Published%'");            
-                                    
-            }
-            else{
-		    $pl->filter(false,"ak_group_status like '%Published%' or ak_group_status like '%Ready%'");           
-	        }					
-			}
-			
-			if($template=='pronews_list'){	
+
+			if($template=='pronews_list'){
 			global $u;
 			$category = $this->category;
 			if (!$u -> isLoggedIn ()) {
-            
-            $pl->filter(false,"((ak_group_status like '%Published%') and (ak_regional_feature not like '%".$category."%' or ak_regional_feature is NULL ))"); 
-            //$pl->filter(false,"ak_regional_feature not like '%$category%'");           
-            //$pl->filterByAttribute('regional_feature',"%$category%",'not like');                         
-            }
-            else{
-            $pl->filter(false,"((ak_group_status like '%Published%' or ak_group_status like '%Ready%') and (ak_regional_feature not like '%".$category."%' or ak_regional_feature is NULL ))");
-		    //$pl->filter(false,"((ak_group_status like '%Published%' or ak_group_status like '%Ready%') and (ak_regional_feature not like '%".$category."%'))");
-		    //$pl->filter(false,"ak_regional_feature not like '%$category%'");
-		    //$pl->filterByAttribute('regional_feature',"%$category%",'not like');	            
-	        }
-           					
-			}
-			
-            if($template=='search'){        
-            		
-			global $u;
-            if (!$u -> isLoggedIn ()) {
-            $pl->filter(false,"ak_group_status like '%Published%'");            
-                                    
-            }
-            else{
-		   $pl->filter(false,"ak_group_status like '%Published%' or ak_group_status like '%Ready%'");	            
-	        }	
-	               					
-			}
-			
-			if($template=='full_article'){        
-            		
-			global $u;
-            if (!$u -> isLoggedIn ()) {
-            $pl->filter(false,"ak_group_status like '%Published%'");            
-                                    
-            }
-            else{
-		    $pl->filter(false,"(ak_group_status like '%Published%' or ak_group_status like '%Ready%')");	            
-	        }	        					
-			}
-			
 
+                            $pl->filter(false,"((ak_group_status like '%Published%') and (ak_regional_feature not like '%".$category."%' or ak_regional_feature is NULL ))");
+                            //$pl->filter(false,"ak_regional_feature not like '%$category%'");
+                            //$pl->filterByAttribute('regional_feature',"%$category%",'not like');
+                            }
+                            else{
+                            $pl->filter(false,"((ak_group_status like '%Published%' or ak_group_status like '%Ready%') and (ak_regional_feature not like '%".$category."%' or ak_regional_feature is NULL ))");
+                                    //$pl->filter(false,"((ak_group_status like '%Published%' or ak_group_status like '%Ready%') and (ak_regional_feature not like '%".$category."%'))");
+                                    //$pl->filter(false,"ak_regional_feature not like '%$category%'");
+                                    //$pl->filterByAttribute('regional_feature',"%$category%",'not like');
+                                }
+
+			}
+
+                        if($template=='search') {
+
+                            global $u;
+                            if (!$u -> isLoggedIn ()) {
+                            $pl->filter(false,"ak_group_status like '%Published%'");
+                            } else {
+                                $pl->filter(false,"ak_group_status like '%Published%' or ak_group_status like '%Ready%'");
+                            }
+                        }
+
+			if($template=='full_article'){
+                            global $u;
+                            if (!$u -> isLoggedIn ()) {
+                            $pl->filter(false,"ak_group_status like '%Published%'");
+
+                            } else {
+                                    $pl->filter(false,"(ak_group_status like '%Published%' or ak_group_status like '%Ready%')");
+                            }
+			}
+
+                        //we want to get the first page of the whole list before
+                        //going and getting the specific page that is being requested
+                        $first_page = clone $pl;
+
+                        //returns a single item array
+                        $first_page = $first_page->get(1, 0);
+                        $this->set('first_page', $first_page[0]);
+
+                        //now go and get pages
 			if ($num > 0) {
 				$pages = $pl->getPage();
 			} else {
 				$pages = $pl->get();
 			}
-			
-			if($template=='search' && $_GET['q'] != ''){ 
-			$pages = $pl->get();
+
+			if($template=='search' && $_GET['q'] != ''){
+                            $pages = $pl->get();
 			}
 			$this->set('pl', $pl);
 			return $pages;
 		}
-		
+
 		public function view() {
 			$cArray = $this->getPages();
 			$nh = Loader::helper('navigation');
 			$this->set('nh', $nh);
-			//$this->set('cArray', $cArray);
 			$this->set('cArray', $cArray);
 		}
-		
+
 		public function add() {
 			Loader::model("collection_types");
 			$c = Page::getCurrentPage();
@@ -272,14 +266,14 @@
 			$this->set('bt', BlockType::getByHandle('pronews_list'));
 			$this->set('displayAliases', true);
 		}
-	
+
 		public function edit() {
 			$b = $this->getBlockObject();
 			$bCID = $b->getBlockCollectionID();
 			$bID=$b->getBlockID();
 			$this->set('bID', $bID);
 			$c = Page::getCurrentPage();
-			if ($c->getCollectionID() != $this->cParentID && (!$this->cThis) && ($this->cParentID != 0)) { 
+			if ($c->getCollectionID() != $this->cParentID && (!$this->cThis) && ($this->cParentID != 0)) {
 				$isOtherPage = true;
 				$this->set('isOtherPage', true);
 			}
@@ -287,7 +281,7 @@
 			$this->set('uh', $uh);
 			$this->set('bt', BlockType::getByHandle('pronews_list'));
 		}
-		
+
 		function save($args) {
 			// If we've gotten to the process() function for this class, we assume that we're in
 			// the clear, as far as permissions are concerned (since we check permissions at several
@@ -299,23 +293,23 @@
 			if (is_object($c)) {
 				$this->cID = $c->getCollectionID();
 			}
-			
+
 			$args['num'] = ($args['num'] > 0) ? $args['num'] : 0;
 			$args['cThis'] = ($args['cParentID'] == $this->cID) ? '1' : '0';
 			$args['cParentID'] = ($args['cParentID'] == 'OTHER') ? $args['cParentIDValue'] : $args['cParentID'];
 			$args['truncateSummaries'] = ($args['truncateSummaries']) ? '1' : '0';
 			$args['displayFeaturedOnly'] = ($args['displayFeaturedOnly']) ? '1' : '0';
 			$args['displayAliases'] = ($args['displayAliases']) ? '1' : '0';
-			$args['truncateChars'] = intval($args['truncateChars']); 
-			$args['paginate'] = intval($args['paginate']); 
+			$args['truncateChars'] = intval($args['truncateChars']);
+			$args['paginate'] = intval($args['paginate']);
 			$args['category'] = isset($args['category']) ? $args['category'] : '';
 			$args['use_content'] = ($args['use_content']) ? '1' : '0';
 			$args['title'] = isset($args['title']) ? $args['title'] : '';
 
 			parent::save($args);
-		
+
 		}
-		
+
 		function getBlockPath($path){
 			$db = Loader::db();
 			$r = $db->Execute("SELECT cPath FROM PagePaths WHERE cID = '$path' ");
