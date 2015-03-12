a656c5c3cd9d952c2259847aa4acb9d92d434846
Kevin Wilson <kevinwwilson@yahoo.com>
2015-03-12 01:00:37 -0400

    Refactor publish checking to use single query

---
+++ helpers/news_publish.php
@@ -0,0 +1,77 @@
+<?php
+
+defined('C5_EXECUTE') or die("Access Denied.");
+
+class NewsPublishHelper {
+
+    /**
+     * Checks the news groups to see if there is a group that needs to be published
+     * 
+     * @return bool true if there is a group that is ready for publishing
+     */
+    public function groupReadyToPublish() {
+        $db = Loader::db();		 
+        $newsGroups = $db->GetArray("SELECT * FROM `btselectProNewsList` order by time desc");
+        $current_time = date("Y-m-d H:i:s");
+        foreach ($newsGroups as $group) {
+            // as soon as we find a published group, the search is over
+            if ($group['active'] == 1) {
+//                var_dump('not publishing'); die();
+                return false;
+            }
+            
+            if ($group['active'] == 0 && $current_time > $group['time'])  {
+                return true;
+            }
+        }
+        var_dump('not publishing2'); die();
+        return false;
+    }
+    
+    /**
+     * Publishes a news group
+     * 
+     * @return null
+     */
+    public function publishGroup() {
+//        var_dump('publishing'); die();
+        $db = Loader::db();		 
+        $newsGroups = $db->GetArray("SELECT * FROM `btselectProNewsList` order by time desc");
+        $current_time = date("Y-m-d H:i:s");
+        $lesstime = array();
+        foreach($newsGroups as $data){
+            $curid = $data['ID']; 
+            $ctime = $data['time'];
+                if($current_time > $ctime){
+                    array_push($lesstime, $ctime);				
+                }
+        }
+        $largevalue = max($lesstime);
+        $sqlz = $db->query("UPDATE btselectProNewsList SET active='0'");
+        $db->Execute($sqlz);
+        $sql = $db->query("UPDATE btselectProNewsList SET active='1' WHERE time='$largevalue'");
+        $db->Execute($sql);
+
+        $rows = $db->GetArray("SELECT * FROM btselectProNewsList WHERE time='$largevalue'");
+
+        foreach($rows as $actdata){
+            $curids = $actdata['atID'];	
+            $idsplit = explode('||', $curids);
+            foreach($idsplit as $atids)
+            {
+                $page = Page::getByID($atids, $version = 'RECENT');
+                $page->setAttribute('group_status', 'Published'); 
+        //	Loader::model('page_list');
+        //    $newsSectionList = new PageList();
+
+        //    $spages = $newsSectionList->get(1000);
+        //    foreach($spages as $scpage){    
+        //    if($scpage->cID == $atids){    
+        //    $scpage->setAttribute('group_status','Published');
+        //    }   
+
+            }		
+        }
+    }
+    
+}
--- libraries/change_notifications.php
+++ libraries/change_notifications.php
@@ -1,49 +1,6 @@
 <?php defined('C5_EXECUTE') or die("Access Denied.");
 
-$db = Loader::db();		 
-$row = $db->GetArray("SELECT * FROM `btselectProNewsList` order by time desc");
-$current_time = date("Y-m-d H:i:s");
-
-if ($current_time < $row[0]['time'] || $row[0]['active'] == 1) {
-    var_dump('doing nothing');
-    // nothing to publish, don't do anything
-} else {
-    $lesstime = array();
-    foreach($row as $data){
-     $curid = $data['ID']; 
-     $ctime = $data['time']; 
-
-                    if($current_time > $ctime){
-
-                    array_push($lesstime, $ctime);				
-    }
-    }
-    $largevalue = max($lesstime);
-    $sqlz = $db->query("UPDATE btselectProNewsList SET active='0'");
-    $db->Execute($sqlz);
-    $sql = $db->query("UPDATE btselectProNewsList SET active='1' WHERE time='$largevalue'");
-    $db->Execute($sql);
-
-    $rows = $db->GetArray("SELECT * FROM btselectProNewsList WHERE time='$largevalue'");
-
-    foreach($rows as $actdata){
-        $curids = $actdata['atID'];	
-        $idsplit = explode('||', $curids);
-        foreach($idsplit as $atids)
-        {
-            $page = Page::getByID($atids, $version = 'RECENT');
-            $page->setAttribute('group_status', 'Published'); 
-    //	Loader::model('page_list');
-    //    $newsSectionList = new PageList();
-
-    //    $spages = $newsSectionList->get(1000);
-    //    foreach($spages as $scpage){    
-    //    if($scpage->cID == $atids){    
-    //    $scpage->setAttribute('group_status','Published');
-    //    }   
-
-        }		
-    }
-}
-
-
+$publishHelper = Loader::helper('news_publish');
+if ($publishHelper->groupReadyToPublish()) {
+    $publishHelper->publishGroup();
+} 
