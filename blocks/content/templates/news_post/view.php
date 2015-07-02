c11f6895b742a45209a52b5f88fb71790de5f1c4
kevinwwilson <kwilson@foxbright.com>
2015-07-02 19:56:11 -0400

    Added tumblr to share links

--- blocks/content/templates/news_post/view.php
+++ blocks/content/templates/news_post/view.php
@@ -164,6 +164,7 @@ foreach ($district as $d) {
             <span class='st_linkedin_large' displayText='LinkedIn'></span>
             <span class='st_pinterest_large' displayText='Pinterest'></span>
             <span class='st_googleplus_large' displayText='Google +'></span>
+            <span class='st_tumblr_large' displayText='Tumblr'></span>
             <span class='st_sharethis_large' displayText='ShareThis'></span>
             <span class='st_email_large' displayText='Email'></span>
             <span class='st_fblike_large' displayText='Facebook Like'></span>
@@ -216,6 +217,7 @@ foreach ($district as $d) {
             <span class='st_linkedin_large' displayText='LinkedIn'></span>
             <span class='st_pinterest_large' displayText='Pinterest'></span>
             <span class='st_googleplus_large' displayText='Google +'></span>
+            <span class='st_tumblr_large' displayText='Tumblr'></span>
             <span class='st_sharethis_large' displayText='ShareThis'></span>
             <span class='st_email_large' displayText='Email'></span>
             <span class='st_fblike_large' displayText='Facebook Like'></span>
