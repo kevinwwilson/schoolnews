6becd04d2d64537a98e576f1fe210cddddbf3384
Kevin Wilson <kevinwwilson@yahoo.com>
2015-08-09 23:14:24 -0400

    Refactored feed creation into separate methods

--- controllers/rss_feed.php
+++ controllers/rss_feed.php
@@ -5,56 +5,11 @@ class RssFeedController extends Controller  {
 
     
      public function view() {
-         
-        Loader::helper('get_news_info');
-        Loader::library('FeedWriter/Item');
-        Loader::library('FeedWriter/Feed');
-        Loader::library('FeedWriter/RSS2');
-        //Creating an instance of RSS1 class.
-
-        date_default_timezone_set('UTC');
-
-        $Feed = new \FeedWriter\RSS2();
-
-        //Setting the channel elements
-        //Use wrapper functions for common elements
-        //For other optional channel elements, use setChannelElement() function
-        $Feed->setTitle(SITE);
-        $Feed->setLink(BASE_URL);
-        $Feed->setDate(new DateTime());
-        //Set feed description to the home page description
-        $home = Page::getByID(1);
-        $Feed->setDescription($home->getAttribute('meta_description'));
-        $Feed->addNamespace('media', 'http://search.yahoo.com/mrss/');
-        //It's important for RSS 1.0 
-        $Feed->setChannelAbout(BASE_URL . '/about');
-
-        //Adding a feed. Generally this portion will be in a loop and add all feeds.
-        $newsArticleList = GetNewsInfoHelper::getRecentNews();
-//var_dump($newsArticleList); die();
-        foreach ($newsArticleList as $article) {
-            //Create an empty FeedItem
-            $newItem = $Feed->createNewItem();                  
-            //Add elements to the feed item
-            //Use wrapper functions to add common feed elements
-            $newItem->setTitle(strip_tags($article->title));
-            $newItem->setLink($article->link);
-            
-            $newItem->setDate($article->date);
-                        
-            $desc = "<em>" . $this->parseDistrict($article->dateline) . ", MI &#151; </em>";
-            $desc .= strip_tags($article->summary, "<br>,<em>,<strong>");
-            
-            $newItem->setDescription($desc);
-//            $newItem->setAuthor($article->author);
-            $newItem->addElement('media:content', null, array('url'=>$article->thumbnail));
-            //Now add the feed item
-            $Feed->addItem($newItem);
-
-        }
+        
+        $feed = $this->createFeed();
 
         //OK. Everything is done. Now generate the feed.
-        $this->set('feed',$Feed);
+        $this->set('feed',$feed);
     }
     
     /*
@@ -70,6 +25,59 @@ class RssFeedController extends Controller  {
         } else {
             return 'Kent ISD';
         }
+    }
+    
+    public function createFeed() {
+        Loader::helper('get_news_info');
+        Loader::library('FeedWriter/Item');
+        Loader::library('FeedWriter/Feed');
+        Loader::library('FeedWriter/RSS2');
+        //Creating an instance of RSS1 class.
+
+        date_default_timezone_set('UTC');
+
+        $feed = new \FeedWriter\RSS2();
+
+        //Setting the channel elements
+        //Use wrapper functions for common elements
+        //For other optional channel elements, use setChannelElement() function
+        $feed->setTitle(SITE);
+        $feed->setLink(BASE_URL);
+        $feed->setDate(new DateTime());
+        //Set feed description to the home page description
+        $home = Page::getByID(1);
+        $feed->setDescription($home->getAttribute('meta_description'));
+        $feed->addNamespace('media', 'http://search.yahoo.com/mrss/');
+        //It's important for RSS 1.0 
+        $feed->setChannelAbout(BASE_URL . '/about');
+
+        //Adding a feed. Generally this portion will be in a loop and add all feeds.
+        $newsArticleList = GetNewsInfoHelper::getRecentNews();
+        
+        foreach ($newsArticleList as $article) {
+            $this->addFeedItem($feed, $article);
+        }
+        return $feed;
+    }
+    
+    public function addFeedItem($feed, $article) {
+        //Create an empty FeedItem
+        $newItem = $feed->createNewItem();                  
+        //Add elements to the feed item
+        //Use wrapper functions to add common feed elements
+        $newItem->setTitle(strip_tags($article->title));
+        $newItem->setLink($article->link);
+
+        $newItem->setDate($article->date);
+
+        $desc = "<em>" . $this->parseDistrict($article->dateline) . ", MI &#151; </em>";
+        $desc .= strip_tags($article->summary, "<br>,<em>,<strong>");
+
+        $newItem->setDescription($desc);
+
+        $newItem->addElement('media:content', null, array('url'=>$article->thumbnail));
+        //Now add the feed item
+        $feed->addItem($newItem);
     }
 }
 ?>
