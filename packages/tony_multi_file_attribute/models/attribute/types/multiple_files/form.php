9151e6c3c0beec72e8caeb1320591d0c5cf5184e
Kevin <kevinwwilson@yahoo.com>
2016-09-10 22:12:03 -0600

    Fixed multiple files attribute for php 5-6 errors

--- packages/tony_multi_file_attribute/models/attribute/types/multiple_files/form.php
+++ packages/tony_multi_file_attribute/models/attribute/types/multiple_files/form.php
@@ -1,72 +1,71 @@
-  
-<script>
-function assignChooseMultiFileAttrFunc<?=$this->attributeKey->getAttributeKeyID() ?>(){
 
+<script>
+function assignChooseMultiFileAttrFunc<?php echo $this->attributeKey->getAttributeKeyID() ?>(){
 	ccm_chooseAsset = function (data){
-		if(!parseInt(data.fID)) return false;  
-		var html = '<li class="fileAttachmentRow" id="ak<?=$this->attributeKey->getAttributeKeyID() ?>_fileAttachmentRow'+data.fID+'">'; 
-		html = html+'<table class="multiple_files"><tr><td class="files_sort"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span></td><td class="files_file"><input name="akID[<?=$this->attributeKey->getAttributeKeyID() ?>][fID][]" type="checkbox" checked="checked" value="'+data.fID+'" />'; 
+		if(!parseInt(data.fID)) return false;
+		var html = '<li class="fileAttachmentRow" id="ak<?php echo $this->attributeKey->getAttributeKeyID() ?>_fileAttachmentRow'+data.fID+'">';
+		html = html+'<table class="multiple_files"><tr><td class="files_sort"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span></td><td class="files_file"><input name="akID[<?php echo $this->attributeKey->getAttributeKeyID() ?>][fID][]" type="checkbox" checked="checked" value="'+data.fID+'" />';
 		html = html+'<a class="fileAttachmentTitle" href="'+data.filePathDirect+'" target="_blank">'+data.title+'</a></td>';
-		html = html+'<td class="files_caption"><input type="text" placeholder="Caption" name="akID[<?=$this->attributeKey->getAttributeKeyID() ?>][fName][][[' + data.fID + ']" /></td> </tr></table></li>'; 
-		$('#ak<?=$this->attributeKey->getAttributeKeyID() ?>_attachedFilesList').append(html); 
+		html = html+'<td class="files_caption"><input type="text" placeholder="Caption" name="akID[<?php echo $this->attributeKey->getAttributeKeyID() ?>][fName][][[' + data.fID + ']" /></td> </tr></table></li>';
+		$('#ak<?php echo $this->attributeKey->getAttributeKeyID() ?>_attachedFilesList').append(html);
 	}
-	
+
 }
 
 </script>
 
 
-<a onclick="assignChooseMultiFileAttrFunc<?=$this->attributeKey->getAttributeKeyID() ?>();ccm_alLaunchSelectorFileManager(''); return false" href="#"><?=t('Add a Image/File &raquo;') ?></a>
-  
-<ul id="ak<?=$this->attributeKey->getAttributeKeyID() ?>_attachedFilesList" class="filesList"> 
- 
-<?  
+<a onclick="assignChooseMultiFileAttrFunc<?php echo $this->attributeKey->getAttributeKeyID() ?>();ccm_alLaunchSelectorFileManager(''); return false" href="#"><?php echo t('Add a Image/File &raquo;') ?></a>
+
+<ul id="ak<?php echo $this->attributeKey->getAttributeKeyID() ?>_attachedFilesList" class="filesList">
+
+<?php
 
 if (is_object($this->attributeValue)){
 	$multiFilesValueObj = $this->attributeValue->getValue().'';
 	//echo get_class( $fIDsStr );
-	//$dirtyFIDs = explode(',',$fIDsStr); 
-	foreach(MultipleFilesAttributeTypeController::getFiles($multiFilesValueObj) as $fileObj){ 
+	//$dirtyFIDs = explode(',',$fIDsStr);
+	foreach(MultipleFilesAttributeTypeController::getFiles($multiFilesValueObj) as $fileObj){
 	$file = $fileObj[0];
 	$fileName = $fileObj[1];
 		$fv = $file->getApprovedVersion();
 		?>
-    <li class="fileAttachmentRow" id="ak<?=$this->attributeKey->getAttributeKeyID() ?>_fileAttachmentRow<?= $file->getFileID() ?>">
+    <li class="fileAttachmentRow" id="ak<?php echo $this->attributeKey->getAttributeKeyID() ?>_fileAttachmentRow<?php echo $file->getFileID() ?>">
         <table class="multiple_files">
             <tr>
                 <td class="files_sort">
                     <span class="ui-icon ui-icon-arrowthick-2-n-s"></span>
                 </td>
                 <td class="files_file">
-                    <input name="akID[<?=$this->attributeKey->getAttributeKeyID() ?>][fID][]" type="checkbox" checked="checked" value="<?= $file->getFileID() ?>" /> 
-                    <a class="fileAttachmentTitle" href="<?= $fv->getRelativePath() ?>" target="_blank"><?= $fv->getTitle() ?></a>
+                    <input name="akID[<?php echo$this->attributeKey->getAttributeKeyID() ?>][fID][]" type="checkbox" checked="checked" value="<?php echo $file->getFileID() ?>" />
+                    <a class="fileAttachmentTitle" href="<?php echo $fv->getRelativePath() ?>" target="_blank"><?php echo $fv->getTitle() ?></a>
                 </td>
                 <td class="files_caption">
-                    <input type="text" placeholder="Caption" name="akID[<?=$this->attributeKey->getAttributeKeyID() ?>][fName][][<?= $file->getFileID() ?>]" value="<?php echo $fileName; ?>" />
-                </td>   
+                    <input type="text" placeholder="Caption" name="akID[<?php echo $this->attributeKey->getAttributeKeyID() ?>][fName][][<?php echo $file->getFileID() ?>]" value="<?php echo $fileName; ?>" />
+                </td>
             </tr>
         </table>
-    </li> 
-	<? }  
-} ?> 
+    </li>
+	<?php }
+} ?>
 
-</ul> 
+</ul>
 
 <script type="text/javascript">
 // drag and drop change_order.php dashboard js
-$(document).ready(function(){ 
-						   
+$(document).ready(function(){
+
 	$(function() {
-		$("#ak<?=$this->attributeKey->getAttributeKeyID() ?>_attachedFilesList").sortable({ opacity: 0.6, cursor: 'move', update: function() {
-			var order = $(this).sortable("serialize"); 
+		$("#ak<?php echo $this->attributeKey->getAttributeKeyID() ?>_attachedFilesList").sortable({ opacity: 0.6, cursor: 'move', update: function() {
+			var order = $(this).sortable("serialize");
 			$.post("<?php  echo $this->action('ajax'); ?>", order, function(theResponse){
-				$("#status").html(theResponse);				
-			}); 															 
-		}								  
+				$("#status").html(theResponse);
+			});
+		}
 		});
 	});
- 
-});	
+
+});
 </script>
 <style type="text/css">
     .fileAttachmentRow {
@@ -87,22 +86,21 @@ $(document).ready(function(){
     .filesList .multiple_files  {
         width: 100%;
     }
-    
+
     .multiple_files .files_sort {
         width: 3%;
     }
-    
+
     .multiple_files .files_file {
         width: 37%;
     }
-    
+
     .multiple_files .files_caption input {
         width: inherit;
     }
-    
+
     .multiple_files .files_caption {
         width: 98%;
     }
 
 </style>
-
