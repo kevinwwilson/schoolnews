4173ce1edfc304f5ee3d38a1beb53a05694885f6
Kevin <kevinwwilson@yahoo.com>
2016-10-17 22:26:05 -0600

    Refactored loader into commented helper class

--- tools/feed/loader.php
+++ tools/feed/loader.php
@@ -2,53 +2,8 @@
 defined('C5_EXECUTE') or die(_("Access Denied."));
 $type = strtolower($_REQUEST['type']);
 $name = $_REQUEST['name'];
-$error = false;
-
-//get custom php generated content
-if ($type == 'custom') {
-    ob_start();
-    $result = include 'custom\\'. $name . '.php';
-    if ($result) {
-        $html = ob_get_clean();
-    } else {
-        $html = '<p class="error"> Unknown feed name: ' . $name . '</p>';
-        $error = true;
-    }
-    //assign all captured output to variable
-} elseif ($type == 'stack') {
-    //get stack
-    $stack = Stack::getByName($name);
-    if ($stack) {
-        $blocks = $stack->getBlocks();
-        //start capturing output stream
-        ob_start();
-        foreach ($blocks as $block) {
-            $block->display();
-        }
-        //assign all captured output to variable
-        $html = ob_get_clean();
-    } else {
-        $html = '<p class="error"> Unknown stack name: ' . $name . '</p>';
-        $error = true;
-    }
-
-} else {
-    $html = '<p class="error"> Expected feed type of custom or stack and found ' . $type . '</p>';
-    $error = true;
-}
-
-//keep the images from loading when first brought into the browsers
-$html = str_replace('src="', 'src-data="', $html);
-
-$returnData = [];
-if ($error) {
-    $returnData['status'] = 'Error';
-} else {
-    $returnData['status'] = 'Success';
-}
-
-$returnData['data'] = $html;
-$json = json_encode($returnData);
+$loaderHelper = Loader::helper('load_feed');
+$json = $loaderHelper->getFeed($type, $name);
 
 header('Content-Type: application/javascript');
 echo $_GET['callback']."(";
